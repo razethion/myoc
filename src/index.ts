852f1938dae1ca01b5aba2ad151e3a631a4846f9
razeth <r@zeth.pw>
2026-06-10 17:27:45 -0500

    cleanup duplicates add login api flow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
+import { apiRoutes } from './routes/api'
 import { pageRoutes } from './routes/pages'
+import type { Bindings } from './types/bindings'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>()
 
+app.route('/api', apiRoutes)
 app.route('/', pageRoutes)
 
 // Cloudflare Workers loads this default export from wrangler.jsonc.
